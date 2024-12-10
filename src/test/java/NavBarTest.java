1ba3319e4ed4ad78df170e2d292eb92425da8a1a
Aji Fauzi P <ajifauzi919@gmail.com>
2024-12-10 10:55:10 +0700

    add navResetState function to verify Reset State button

--- src/test/java/NavBarTest.java
+++ src/test/java/NavBarTest.java
@@ -1,6 +1,9 @@
 import base.BaseUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -8,9 +11,12 @@ import org.testng.annotations.Test;
 import page.LoginPage;
 import page.NavBarPage;
 
+import java.time.Duration;
+
 public class NavBarTest {
     WebDriver driver;
     BaseUtil baseUtil = new BaseUtil();
+    Duration duration = Duration.ofSeconds(10);
 
     @BeforeClass
     public void setup() {
@@ -49,10 +55,25 @@ public class NavBarTest {
     @Test(priority = 3)
     public void navResetStateTest() {
         NavBarPage navBarPage = new NavBarPage(driver);
-        // click nav menu
-        navBarPage.clickNavMenuBtn();
-        // click reset state button
-        navBarPage.clickResetStateBtn();
+        // locator "Add to cart" button
+        By addToCartBtn = By.xpath("//*[contains(text(),'Add to cart')]");
+        // locator "Remove" button
+        By removeBtn = By.xpath("//*[contains(text(),'Remove')]");
+
+        driver.findElement(addToCartBtn).click();
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(removeBtn)
+        );
+
+        double totalCart = Double.parseDouble(driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText());
+        System.out.println("jumlah cart saat ini : " + totalCart);
+        if (totalCart > 0) {
+            // click nav menu
+            navBarPage.clickNavMenuBtn();
+            // click reset state button
+            navBarPage.clickResetStateBtn();
+        }
     }
 
     @Test(priority = 4)
